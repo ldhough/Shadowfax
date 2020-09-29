1128e227e51fc3791913fc83d047f65f2a8d6096
ldhough <ldhough2000@gmail.com>
2020-09-28 22:19:34 -0400

    Initial commit

---
+++ Shadowfax/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shadowfax
+//
+//  Created by Lannie Hough on 9/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
